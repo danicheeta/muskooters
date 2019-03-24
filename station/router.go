aab80edc1beb9b40a7d280993498bf78689cf2c9
daniel <danicheeta@yahoo.com>
2019-03-24 12:39:34 +0430

    new scooter router

--- station/router.go
+++ station/router.go
@@ -1,1 +1,28 @@
 package station
+
+import (
+	"github.com/gin-gonic/gin"
+	"muskooters/user/middleware"
+	"muskooters/services/framework"
+	"muskooters/user"
+	"net/http"
+)
+
+type Route struct {}
+
+func (Route) Routes(e *gin.Engine) {
+	g := e.Group("scooter")
+	g.Use(middleware.FetchToken)
+	g.POST("register", middleware.Auth(user.Zeus), registerScooter)
+}
+
+func registerScooter(c *gin.Context) {
+	s := NewScooter()
+	c.JSON(http.StatusOK, s)
+}
+
+
+
+func init(){
+	framework.Register(Route{})
+}
--- station/skooter.go
+++ station/skooter.go
@@ -1,1 +1,30 @@
 package station
+
+import (
+	"github.com/globalsign/mgo/bson"
+	"muskooters/services/mongo"
+	"muskooters/services/assert"
+	"github.com/globalsign/mgo"
+	"errors"
+)
+
+const collectionName = "scooters"
+
+var starterState = Ready
+
+type Scooter struct {
+	ID    bson.ObjectId `bson:"_id"`
+	State State
+}
+
+func NewScooter() Scooter {
+	id := bson.NewObjectId()
+	scooter := Scooter{ID: id, State: starterState}
+
+	c := mongo.GetDB().C(collectionName)
+	err := c.Insert(scooter)
+	assert.Nil(err)
+
+	return scooter
+}
+
