499fa8106f1d82ce05bde04b1bb6144f46bb9d7c
daniel <danicheeta@yahoo.com>
2019-03-24 15:11:39 +0430

    graph wraper of transitions

--- station/state.go
+++ station/state.go
@@ -1,17 +1,12 @@
 package station
 
-import "muskooters/user"
-
-type State int
-
-type Transitions struct {
-	From  State
-	To    State
-	Roles []user.Role
-}
+import (
+	"fmt"
+	"muskooters/user"
+)
 
 const (
-	Ready      State = iota
+	Ready State = iota
 	BatteryLow
 	Bounty
 	Riding
@@ -19,13 +14,24 @@ const (
 	Dropped
 )
 
+type State int
+
+type Transition struct {
+	From  State
+	To    State
+	Roles []user.Role
+}
+
 var (
 	csRoles = []user.Role{user.Client, user.Scooter}
 	sRoles  = []user.Role{user.Scooter}
 	hRoles  = []user.Role{user.Hunter}
+
+	hashMap = map[State]map[State][]user.Role{}
 )
 
-var graph = []Transitions{
+// Default transitions
+var graph = []Transition{
 	{From: Ready, To: Riding, Roles: csRoles},
 	{From: Ready, To: Bounty, Roles: sRoles},
 	{From: Riding, To: Ready, Roles: csRoles},
@@ -34,4 +40,17 @@ var graph = []Transitions{
 	{From: Bounty, To: Collected, Roles: hRoles},
 	{From: Collected, To: Dropped, Roles: hRoles},
 	{From: Dropped, To: Ready, Roles: hRoles},
+}
+
+// TODO error handling
+// wrap to query easier
+func init() {
+	for _, t := range graph {
+		from := t.From
+		if f, ok := hashMap[from]; ok {
+			f[t.To] = t.Roles
+		} else {
+			hashMap[from] = map[State][]user.Role{t.To: t.Roles}
+		}
+	}
 }
